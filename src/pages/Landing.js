b68d85a71725740612d0eeebe635fe7ad24d28c3
neerajnerlekar <neeraj.nerlekar@gmail.com>
2023-03-16 05:43:08 -0600

    add initial landing page with button and main image

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -2,6 +2,26 @@ import logo from '../assets/images/logo.svg'
 import main from '../assets/images/main.svg'
 
 const Landing = () => {
-  return <h1>Landing page</h1>
+  return (
+    <main>
+      <nav>
+        <img src={logo} alt="jobster logo" className="logo" />
+      </nav>
+      <div className="container page">
+        {/* info */}
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad, sunt
+            quasi placeat labore esse hic debitis odio tempora adipisci nulla!
+          </p>
+          <button className="btn btn-hero">login/register</button>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </main>
+  )
 }
 export default Landing
