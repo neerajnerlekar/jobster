a68eddaaa80c9761e5b866f89ba836bfcf05e247
neerajnerlekar <neeraj.nerlekar@gmail.com>
2023-03-23 12:52:58 -0600

    step 33 - import wrappers in navbar, smallSidebar and BigSidebar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,10 @@
+import Wrapper from '../assets/wrappers/Navbar'
+
 const Navbar = () => {
-  return <h2>Navbar</h2>
+  return (
+    <Wrapper>
+      <h2>Navbar</h2>
+    </Wrapper>
+  )
 }
 export default Navbar
