c266a45180b4313ca3117292f6b1ddd5d38c1a8c
neerajnerlekar <neeraj.nerlekar@gmail.com>
2023-03-24 04:21:35 -0600

    step 48 - if status 401, logout user

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -46,6 +46,10 @@ export const updateUser = createAsyncThunk(
       })
       return resp.data
     } catch (error) {
+      if (error.response.status === 401) {
+        thunkAPI.dispatch(logoutUser())
+        return thunkAPI.rejectWithValue('Unauthorized! Logging out....')
+      }
       return thunkAPI.rejectWithValue(error.response.data.msg)
     }
   }
